9c809283a9b5578ecd7d95fbd12fbf732ab65710
Mario Magdy <mariomagdygad0@gmail.com>
2022-12-09 20:39:57 +0200

    Updates

---
+++ precache-manifest.86761f9f0c4bd738f223416014da9b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b95997a56d14c6658ba3",
+    "url": "/AddGoal-App/static/css/main.fa55d47f.chunk.css"
+  },
+  {
+    "revision": "b95997a56d14c6658ba3",
+    "url": "/AddGoal-App/static/js/main.b95997a5.chunk.js"
+  },
+  {
+    "revision": "1aaa147a804892c77c0c",
+    "url": "/AddGoal-App/static/js/1.1aaa147a.chunk.js"
+  },
+  {
+    "revision": "7e8f1a9250dee570381e",
+    "url": "/AddGoal-App/static/js/runtime~main.7e8f1a92.js"
+  },
+  {
+    "revision": "81313e3e51b336603149ba05f5ecf980",
+    "url": "/AddGoal-App/index.html"
+  }
+];
